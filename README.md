3bc3e9bc6e7f1acbae1864c258550a52a7e17340
andys8 <andys8@users.noreply.github.com>
2017-09-09 15:28:44 +0200

    Webpack, React
    * Based on create-react-app
    * Removed unnecessary build steps
    * Added react app
    * Github pages setup

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # hello-stack
+
+## Frameworks
+React
---
+++ src/index.js
@@ -0,0 +1,12 @@
+// React
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Styles
+import './index.css';
+
+// React App
+import App from './react/App';
+
+// React init
+ReactDOM.render(<App />, document.getElementById('app__react'));
