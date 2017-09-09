691487051a9437c4d3ccb30c3798767c518b6ded
andys8 <andys8@users.noreply.github.com>
2017-09-09 15:28:44 +0200

    Webpack, React
    * Based on create-react-app
    * Removed unnecessary build steps
    * Added react app
    * Github pages setup

---
+++ src/react/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+const name = 'React'
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="app__react__main">
+        Hello, I'm {name}.
+      </div>
+    );
+  }
+}
+
+export default App;
