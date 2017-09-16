8dd4f734fc18eabd8178de4369d5703071cadb8e
andys8 <andys8@users.noreply.github.com>
2017-09-16 22:05:16 +0200

    Angular App

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -99,6 +99,10 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.ContextReplacementPlugin(
+      /angular(\\|\/)core(\\|\/)@angular/,
+      __dirname
+    ),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
