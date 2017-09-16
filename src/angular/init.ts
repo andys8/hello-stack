fd942221c9cac388a209968e7c64a3c8fd2097a2
andys8 <andys8@users.noreply.github.com>
2017-09-16 23:52:03 +0200

    Angular: Production Mode

--- src/angular/init.ts
+++ src/angular/init.ts
@@ -2,5 +2,7 @@ import 'reflect-metadata';
 import 'zone.js';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
+import { enableProdMode } from '@angular/core';
 
+enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
